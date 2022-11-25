aee017d86599fcba0ad8bf2e726518ec81baac95
Mr-Bowtie <mr.bowtie989@gmail.com>
2022-11-25 12:09:39 -0500

    store: changes in progress

--- movement.rb
+++ movement.rb
@@ -7,12 +7,31 @@ class Timer
   end
 
   def run_timer()
+    # make a secondary regular pom timer
+    #TODO have a time checker inside this method to break for lunch and end of day at the right time
+    #TODO use lambdas for Time.now.hour, break time check into method
+    work_finish = Time.now.to_i + work_time
+    break_finish = work_finish + break_time
     puts "Work Timer active"
-    sleep(work_time)
-    # binding.pry
-    popup("work")
+    loop do
+      break if Time.now.to_i >= work_finish || Time.now.hour == 14 || Time.now.hour == 18
+      sleep 60
+    end
+    if Time.now.hour == 14
+      popup("lunch")
+      return
+    elsif Time.now.hour == 18
+      popup("eod")
+      return
+    else
+      popup("work")
+    end
     puts "Break Timer active"
-    sleep(break_time)
+    loop do
+      break if Time.now.to_i >= break_finish || Time.now.hour == 14 || Time.now.hour == 18
+      sleep 60
+    end
+    return if Time.now.hour == 14 || Time.now.hour == 18
     popup("break")
   end
 
@@ -20,48 +39,54 @@ class Timer
     image = ""
     title = ""
     body = ""
+    # turn into switch?
     if process == "work"
       # linux command
       # system("notify-send -u critical -i ~/programming/side-projects/pomodoro/assets/break.jpeg 'Time for a break' 'take a chill Pill'")
       system("osascript -e 'display notification \"Switch desk position & break!\" sound name \"Hero\"'")
-    else
+    elsif process == "lunch"
+      system("osascript -e 'display notification \"Lunch Time!\" sound name \"Submarine\"'")
+    elsif process == "eod"
+      system("osascript -e 'display notification \"Quittin Time\" sound name \"Submarine\"'")
+    elsif process == "break"
       # system("notify-send -u critical -i ~/programming/side-projects/pomodoro/assets/work.png 'Back to work' 'You got this shit'")
       system("osascript -e 'display notification \"Back to it\" sound name \"Submarine\"'")
     end
   end
 end
 
+# loop do
+system("clear")
+#TODO: clean up, break out full day into method
+puts "======= Pomodoro Timer ======="
+# puts "start full day sequence? (y/n)"
+# if gets.chomp == "y"
+work = 50 * 60
+chill = 10 * 60
+timer = Timer.new(work, chill)
 loop do
-  system("clear")
-  #TODO: clean up, break out full day into method
-  puts "======= Pomodoro Timer ======="
-  puts "start full day sequence? (y/n)"
-  if gets.chomp == "y"
-    work = 50 * 60
-    chill = 10 * 60
-    timer = Timer.new(work, chill)
-    loop do
-      hour = Time.now.hour
-      if hour < 14
-        timer.run_timer
-      elsif hour = 14
-        system("osascript -e 'display notification \"Lunch Time!\" sound name \"Submarine\"'")
-        puts "Lunch Time"
-      elsif hour = 18
-        system("osascript -e 'display notification \"Quittin Time\" sound name \"Submarine\"'")
-        puts "Quittin time"
-      end
-      timer.run_timer
-    end
-  else
-    puts "Enter a work time (in minutes)"
-    work = gets.chomp.to_i * 60
-    puts "Enter a break time (in minutes)"
-    chill = gets.chomp.to_i * 60
-    timer = Timer.new(work, chill)
+  hour = Time.now.hour
+  if (hour < 14 || hour > 14)
     timer.run_timer
+  elsif hour = 14
+    puts "Lunch Time"
+    while Time.now.hour == 14
+      sleep 60
+    end
+  elsif hour = 18
+    puts "Quittin time"
+    break
   end
-
-  puts "Run timer again? (y/n)"
-  break if gets.chomp == "n"
 end
+# else
+#   puts "Enter a work time (in minutes)"
+#   work = gets.chomp.to_i * 60
+#   puts "Enter a break time (in minutes)"
+#   chill = gets.chomp.to_i * 60
+#   timer = Timer.new(work, chill)
+#   timer.run_timer
+# end
+
+#   puts "Run timer again? (y/n)"
+#   break if gets.chomp == "n"
+# end
